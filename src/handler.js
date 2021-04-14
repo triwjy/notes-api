ecb0b072159b1e7e18d31656a3430ad8ec604a20
triwjy <ztriwijaya@gmail.com>
2021-04-14 21:56:19 +0800

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,126 @@
+const { nanoid } = require('nanoid')
+const notes = require('./notes')
+
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+ 
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+ 
+  const newNote = {
+    title, tags, body, id, createdAt, updatedAt,
+  };
+ 
+  notes.push(newNote);
+ 
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+ 
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+const getNoteByIdHandler = (request, h) => {
+    const { id } = request.params
+    const note = notes.filter((n) => n.id === id)[0]
+
+    if(note !== undefined) {
+        const response = h.response({
+            status: 'suceess',
+            data: {
+                note,
+            },
+        })
+        return response
+    }
+
+    const response = h.response({
+        status: "fail",
+        message: 'Note not found',        
+    })
+    response.code(404)
+    return response
+}
+
+const editNoteByIdHandler = (request, h) => {
+    const { id } = request.params
+    const { title = 'Untitled', tags, body } = request.payload
+    const updatedAt = new Date().toISOString()
+
+    const index = notes.findIndex((note) => note.id === id)
+
+    if(index !== -1) {
+        notes[index] = {
+            ...notes[index],
+            title,
+            tags,
+            body,
+            updatedAt
+        }
+
+        const response = h.response({
+            status: 'success',
+            message: 'Note updated'
+        })
+        response.code(200)
+        return response
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Fail to update note'
+    })
+    response.code(404)
+    return response
+}
+
+const deleteNoteByIdHandler = (request, h) => {
+    const { id } = request.params
+    
+    const index = notes.findIndex((note) => note.id === id)
+
+    if(index !== -1) {
+        notes.splice(index, 1)
+        const response = h.response({
+            status: 'success',
+            message: 'Note deleted',
+        })
+        response.code = 200
+        return response
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Fail to delete node',
+    })
+    response.code = 404
+    return response
+}
+
+module.exports = {  addNoteHandler, 
+                    getAllNotesHandler, 
+                    getNoteByIdHandler, 
+                    editNoteByIdHandler,
+                    deleteNoteByIdHandler };
